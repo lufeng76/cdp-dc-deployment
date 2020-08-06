cabee5af80c489acccdb2edeb3c1ecf77e83b0f5
fxu1024 <fxu1024@hotmail.com>
2020-08-06 17:43:13 +0800

    validation update

--- install_full_script.sh
+++ install_full_script.sh
@@ -1,9 +1,10 @@
-# Define global variables here
-export USER=etl_user
-export REALM=FENG.COM
-export HOSTNAME=feng-
+##################################################################################
+# Part1: System checks
+##################################################################################
 
-## On ALL nodes
+##=============================
+## Part1: On ALL nodes
+##=============================
 
 # Setup cloudera manager repo
 echo "[cloudera-manager]
@@ -15,9 +16,6 @@ enabled=1
 autorefresh=0
 type=rpm-md" >> /etc/yum.repos.d/cloudera-manager-7_1_1.repo
 rpm --import https://archive.cloudera.com/cm7/7.1.1/redhat7/yum/RPM-GPG-KEY-cloudera
-
-# Download KRB libs
-yum install -y krb5-workstation krb5-libs
 
 # System Pre-Requisites
 systemctl disable firewalld
@@ -37,9 +35,14 @@ echo "vm.swappiness = 1" >> /etc/sysctl.conf
 sysctl vm.swappiness=1
 # echo performance > /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 
-# User creation
-useradd admin
-useradd ${USER}
+
+##################################################################################
+# Part2: Pre-Installation
+##################################################################################
+
+##=============================
+## Part2: On ALL nodes
+##=============================
 
 # Install Java & PostgreSQL connector
 yum -y install java-1.8.0-openjdk-devel
@@ -52,49 +55,10 @@ yum -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_6
 yum -y install https://yum.postgresql.org/10/redhat/rhel-7-x86_64/python3-psycopg2-2.8.5-1.rhel7.x86_64.rpm 
 yum -y install postgresql10
 
-# Create Kudus directories
-mkdir -p /data/kudu/wal 
-chmod 755 /data/kudu/wal
-#chown kudu:kudu /data/kudu/wal
 
-mkdir -p /data/kudu/data 
-chmod 755 /data/kudu/data
-#chown kudu:kudu /data/kudu/data
-
-
-# Generate Keys and Certificates Signing Requests  
-# TODO : Make auto-signed certificates 
-# Only on one node (CM one by default)
-mkdir -p /opt/cloudera/security/pki/
-openssl genrsa -out rootCA.key 4096
-openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 1024 -out rootCA.crt
-keytool -importcert -alias rootca -keystore /opt/cloudera/security/pki/truststore.jks -file /opt/cloudera/security/pki/rootCA.crt
-chmod 444 /opt/cloudera/security/pki/rootCA.crt
-chmod 400 /opt/cloudera/security/pki/rootCA.key
-
-#Copy the rootCA.crt & rootCA.key & truststore.jks to all nodes and play these lines:
-mkdir -p /opt/cloudera/security/pki/
-cp ~/rootCA.* /opt/cloudera/security/pki/
-
-keytool -genkeypair -alias $(hostname -f) -keyalg RSA -keystore /opt/cloudera/security/pki/$(hostname -f).jks -keysize 2048 -dname "CN=$(hostname -f)" -ext san=dns:$(hostname -f),dns:${HOSTNAME}${1}  -storepass cloudera
-keytool -certreq -alias $(hostname -f) -keystore /opt/cloudera/security/pki/$(hostname -f).jks -file /opt/cloudera/security/pki/$(hostname -f).csr -ext san=dns:$(hostname -f),dns:${HOSTNAME}${1}  -storepass cloudera
-openssl x509 -req -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -days 500 -sha256 -in /opt/cloudera/security/pki/$(hostname -f).csr -out /opt/cloudera/security/pki/$(hostname -f).crt
-cat /opt/cloudera/security/pki/rootCA.crt >> /opt/cloudera/security/pki/$(hostname -f).crt
-keytool -importcert -alias $(hostname -f) -keystore /opt/cloudera/security/pki/$(hostname -f).jks -file /opt/cloudera/security/pki/$(hostname -f).crt  -storepass cloudera
-
-keytool -importkeystore -srckeystore /opt/cloudera/security/pki/$(hostname -f).jks -destkeystore /opt/cloudera/security/pki/$(hostname -f).p12 -srcalias $(hostname -f) -srcstoretype jks -deststoretype pkcs12  -storepass cloudera
-openssl pkcs12 -in /opt/cloudera/security/pki/$(hostname -f).p12 -out /opt/cloudera/security/pki/$(hostname -f).pem
-
-ln -s /opt/cloudera/security/pki/$(hostname -f).jks /opt/cloudera/security/pki/keystore.jks 
-ln -s /opt/cloudera/security/pki/$(hostname -f).pem /opt/cloudera/security/pki/key.pem
-
-chmod 444 /opt/cloudera/security/pki/*
-chmod 400 /opt/cloudera/security/pki/rootCA.*
-
-## Only on CM node 
-
-export USER=etl_user
-export REALM=FENG.COM
+##=============================
+## Part2: Only on CM node 
+##=============================
 
 # Install PostgreSQL 10
 yum -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
@@ -133,6 +97,14 @@ sudo -u postgres psql <<EOF
     CREATE DATABASE ranger OWNER rangeradmin ENCODING 'UTF8' TEMPLATE template0;
 EOF
 
+##################################################################################
+# Part3: CM installation
+##################################################################################
+
+##=============================
+## Part3: Only on CM node 
+##=============================
+
 # Install CM & start it
 yum -y install cloudera-manager-agent cloudera-manager-daemons
 yum -y install cloudera-manager-server
@@ -149,71 +121,13 @@ done
 echo "-- Now CM Server is started --"
 
 
-# Install MIT-KDC
-yum -y install krb5-server krb5-libs
-echo "*/admin@${REALM}	 * " > /var/kerberos/krb5kdc/kadm5.acl
-echo "
-defaul_realm=${REALM}
+##################################################################################
+# Part5: Enable_HA
+##################################################################################
 
-[kdcdefaults]
- kdc_ports = 88
- kdc_tcp_ports = 88
-
-[realms]
- ${REALM} = { 
-  acl_file = /var/kerberos/krb5kdc/kadm5.acl
-  dict_file = /usr/share/dict/words
-  admin_keytab = /var/kerberos/krb5kdc/kadm5.keytab
-  max_life = 24h 0m 0s
-  max_renewable_life = 7d 0h 0m 0s
-  supported_enctypes = aes256-cts:normal aes128-cts:normal des3-hmac-sha1:normal arcfour-hmac:normal camellia256-cts:normal camellia128-cts:normal des-hmac-sha1:normal des-cbc-md5:normal des-cbc-crc:normal
- }" > /var/kerberos/krb5kdc/kdc.conf
-
- echo "
- # Configuration snippets may be placed in this directory as well
-includedir /etc/krb5.conf.d/
-
-[logging]
- default = FILE:/var/log/krb5libs.log
- kdc = FILE:/var/log/krb5kdc.log
- admin_server = FILE:/var/log/kadmind.log
-
-[libdefaults]
- dns_lookup_realm = false
- ticket_lifetime = 24h
- renew_lifetime = 7d
- forwardable = true
- default_realm = ${REALM}
- default_tkt_enctypes = des-cbc-md5 des-cbc-crc des3-cbc-sha1
- default_tgs_enctypes = des-cbc-md5 des-cbc-crc des3-cbc-sha1
- permitted_enctypes = des-cbc-md5 des-cbc-crc des3-cbc-sha1
-
-[realms]
-${REALM} = {
-  kdc = $(hostname -f):88
-  admin_server = $(hostname -f):749
-  default_domain = ${REALM,,}
-}
-
-[domain_realm]
-  ${REALM,,} = ${REALM}
-  .${REALM,,} = ${REALM}
-" > /etc/krb5.conf
-
-###### ATTENTION: This requires interaction from the user
-kdb5_util create -r ${REALM} -s
-
-systemctl start krb5kdc.service
-systemctl start kadmin.service
-systemctl enable krb5kdc.service
-systemctl enable kadmin.service
-
-###### ATTENTION: This requires interaction from the user
-kadmin.local
-addprinc admin/admin
-addprinc ${USER}/admin
-ktadd -k /home/${USER}/${USER}.keytab ${USER}/admin@${REALM}
-
+##=============================
+## Part5: Only on CM node 
+##=============================
 
 # Install and configure HA proxy required by HUE, OOZIE & IMPALA
 yum -y install haproxy
@@ -307,8 +221,109 @@ systemctl enable haproxy
 systemctl start haproxy
 
 
+# Download KRB libs
+yum install -y krb5-workstation krb5-libs
 
+# Define global variables here
+export USER=etl_user
+export REALM=FENG.COM
+export HOSTNAME=feng-
+export USER=etl_user
+export REALM=FENG.COM
 
+# Install MIT-KDC
+yum -y install krb5-server krb5-libs
+echo "*/admin@${REALM}	 * " > /var/kerberos/krb5kdc/kadm5.acl
+echo "
+defaul_realm=${REALM}
+
+[kdcdefaults]
+ kdc_ports = 88
+ kdc_tcp_ports = 88
+
+[realms]
+ ${REALM} = { 
+  acl_file = /var/kerberos/krb5kdc/kadm5.acl
+  dict_file = /usr/share/dict/words
+  admin_keytab = /var/kerberos/krb5kdc/kadm5.keytab
+  max_life = 24h 0m 0s
+  max_renewable_life = 7d 0h 0m 0s
+  supported_enctypes = aes256-cts:normal aes128-cts:normal des3-hmac-sha1:normal arcfour-hmac:normal camellia256-cts:normal camellia128-cts:normal des-hmac-sha1:normal des-cbc-md5:normal des-cbc-crc:normal
+ }" > /var/kerberos/krb5kdc/kdc.conf
+
+ echo "
+ # Configuration snippets may be placed in this directory as well
+includedir /etc/krb5.conf.d/
+
+[logging]
+ default = FILE:/var/log/krb5libs.log
+ kdc = FILE:/var/log/krb5kdc.log
+ admin_server = FILE:/var/log/kadmind.log
+
+[libdefaults]
+ dns_lookup_realm = false
+ ticket_lifetime = 24h
+ renew_lifetime = 7d
+ forwardable = true
+ default_realm = ${REALM}
+ default_tkt_enctypes = des-cbc-md5 des-cbc-crc des3-cbc-sha1
+ default_tgs_enctypes = des-cbc-md5 des-cbc-crc des3-cbc-sha1
+ permitted_enctypes = des-cbc-md5 des-cbc-crc des3-cbc-sha1
+
+[realms]
+${REALM} = {
+  kdc = $(hostname -f):88
+  admin_server = $(hostname -f):749
+  default_domain = ${REALM,,}
+}
+
+[domain_realm]
+  ${REALM,,} = ${REALM}
+  .${REALM,,} = ${REALM}
+" > /etc/krb5.conf
+
+###### ATTENTION: This requires interaction from the user
+kdb5_util create -r ${REALM} -s
+
+systemctl start krb5kdc.service
+systemctl start kadmin.service
+systemctl enable krb5kdc.service
+systemctl enable kadmin.service
+
+###### ATTENTION: This requires interaction from the user
+kadmin.local
+addprinc admin/admin
+addprinc ${USER}/admin
+ktadd -k /home/${USER}/${USER}.keytab ${USER}/admin@${REALM}
+
+# Generate Keys and Certificates Signing Requests  
+# TODO : Make auto-signed certificates 
+# Only on one node (CM one by default)
+mkdir -p /opt/cloudera/security/pki/
+openssl genrsa -out rootCA.key 4096
+openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 1024 -out rootCA.crt
+keytool -importcert -alias rootca -keystore /opt/cloudera/security/pki/truststore.jks -file /opt/cloudera/security/pki/rootCA.crt
+chmod 444 /opt/cloudera/security/pki/rootCA.crt
+chmod 400 /opt/cloudera/security/pki/rootCA.key
+
+#Copy the rootCA.crt & rootCA.key & truststore.jks to all nodes and play these lines:
+mkdir -p /opt/cloudera/security/pki/
+cp ~/rootCA.* /opt/cloudera/security/pki/
+
+keytool -genkeypair -alias $(hostname -f) -keyalg RSA -keystore /opt/cloudera/security/pki/$(hostname -f).jks -keysize 2048 -dname "CN=$(hostname -f)" -ext san=dns:$(hostname -f),dns:${HOSTNAME}${1}  -storepass cloudera
+keytool -certreq -alias $(hostname -f) -keystore /opt/cloudera/security/pki/$(hostname -f).jks -file /opt/cloudera/security/pki/$(hostname -f).csr -ext san=dns:$(hostname -f),dns:${HOSTNAME}${1}  -storepass cloudera
+openssl x509 -req -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -days 500 -sha256 -in /opt/cloudera/security/pki/$(hostname -f).csr -out /opt/cloudera/security/pki/$(hostname -f).crt
+cat /opt/cloudera/security/pki/rootCA.crt >> /opt/cloudera/security/pki/$(hostname -f).crt
+keytool -importcert -alias $(hostname -f) -keystore /opt/cloudera/security/pki/$(hostname -f).jks -file /opt/cloudera/security/pki/$(hostname -f).crt  -storepass cloudera
+
+keytool -importkeystore -srckeystore /opt/cloudera/security/pki/$(hostname -f).jks -destkeystore /opt/cloudera/security/pki/$(hostname -f).p12 -srcalias $(hostname -f) -srcstoretype jks -deststoretype pkcs12  -storepass cloudera
+openssl pkcs12 -in /opt/cloudera/security/pki/$(hostname -f).p12 -out /opt/cloudera/security/pki/$(hostname -f).pem
+
+ln -s /opt/cloudera/security/pki/$(hostname -f).jks /opt/cloudera/security/pki/keystore.jks 
+ln -s /opt/cloudera/security/pki/$(hostname -f).pem /opt/cloudera/security/pki/key.pem
+
+chmod 444 /opt/cloudera/security/pki/*
+chmod 400 /opt/cloudera/security/pki/rootCA.*
 
 
 

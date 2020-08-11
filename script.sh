ac5fe3ebb50ca55bda685511e1ef45bfec3b0bd1
fxu1024 <fxu1024@hotmail.com>
2020-08-11 10:30:57 +0800

    cm_installation update

--- script.sh
+++ script.sh
@@ -1,6 +1,1 @@
-yum -y install postgresql-jdbc*
-cp /usr/share/java/postgresql-jdbc.jar /usr/share/java/postgresql-connector-java.jar
-chmod 644 /usr/share/java/postgresql-connector-java.jar
-yum -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
-yum -y install https://yum.postgresql.org/10/redhat/rhel-7-x86_64/python3-psycopg2-2.8.5-1.rhel7.x86_64.rpm
-yum -y install postgresql10
+yum -y install cloudera-manager-agent cloudera-manager-daemons
